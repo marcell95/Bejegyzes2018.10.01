a636ec1239d24e17a7b464b23495c70fbf3dbc33
Petrik Lajos Szg <info@petrik.hu>
2018-10-01 15:08:57 +0200

    mester fájl

---
+++ BejegyzesProject/Bejegyzes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProject
+{
+    class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime szerkesztve;
+        //DateTime letrejott = new DateTime(2018, 10, 1);
+        private DateTime letrejott;
+
+        public void Like() {
+            this.likeok++;
+
+        }
+
+        public void Kiir() {
+            //Console.WriteLine(this.szerzo + " - " + this.likeok + " - " + letrejott + "\nSzerkesztve: " + this.szerkesztve + "\n" + this.tartalom);
+            Console.WriteLine(this.Szerzo + " - " + this.Likeok + " - " + this.Letrejott + "\nSzerkesztve: " + this.Szerkesztve + "\n" + this.Tartalom);
+
+        }
+
+        public string Szerzo
+        {
+            get
+            {
+                return szerzo;
+            }
+            
+        }
+
+        public string Tartalom
+        {
+            get
+            {
+                return tartalom;
+            }
+
+            set
+            {
+                tartalom = value;
+                szerkesztve = DateTime.Now;
+            }
+        }
+
+        public int Likeok
+        {
+            get
+            {
+                return likeok;
+            }
+            
+        }
+
+        public DateTime Letrejott
+        {
+            get
+            {
+                return letrejott;
+            }
+            
+        }
+
+        public DateTime Szerkesztve
+        {
+            get
+            {
+                return szerkesztve;
+            }
+            
+        }
+
+        public Bejegyzes(string szerzo, string tartalom, int likeok)
+        {
+            this.szerzo = szerzo;
+            this.Tartalom = tartalom;
+            this.likeok = likeok;
+            this.szerkesztve = DateTime.Now;
+            this.letrejott = DateTime.Now;
+        }
+    }
+}
---
+++ BejegyzesProject/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Bejegyzes("Szász Marcell Sámuel", "Tartalmat írd ide!", 0);
+            b1.Like();
+            b1.Kiir();
+            //bejegyzes1.Szerzo;
+
+            var bejegyzesek = new List<Bejegyzes>();
+            bejegyzesek.Add(b1);
+            var b2 = new Bejegyzes("Gipsz Jakab","Szabadkőgipszesek",0);
+            bejegyzesek.Add(b2);
+            var b3 = new Bejegyzes("Old Man","Crabonomicon",0);
+            bejegyzesek.Add(b3);
+
+
+            Console.WriteLine("Add meg a bekérendő számok mennyiségét!");
+            int nSzam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bekérendő számok mennyisége: " + nSzam);
+
+             string szerz;
+             string tart;
+             int likekk;
+
+            for (int i = 0; i < nSzam; i++)
+            {
+                Console.WriteLine("Add meg a " + (i + 1) + ". bejegyzést a listába a következő sorrendben: szerző, tartalom, likeok!");
+                szerz = Console.ReadLine();
+                tart = Console.ReadLine();
+                likekk = Convert.ToInt32(Console.ReadLine());
+
+                var b = new Bejegyzes(szerz, tart, likekk);
+                bejegyzesek.Add(b);
+            }
+
+            Random rand = new Random();
+            for (int i = 0; i < bejegyzesek.Count * 20; i++)
+            {
+                bejegyzesek[rand.Next(0, bejegyzesek.Count)].Like();
+            }
+
+            Console.WriteLine("\n------------------------------\n");
+
+            b1.Kiir();
+            b2.Kiir();
+            b3.Kiir();
+
+            Console.WriteLine("\n------------------------------\n");
+
+            bejegyzesek[1].Tartalom= "Új Tartalom.";
+            bejegyzesek[1].Kiir();
+
+            Console.WriteLine("\n------------------------------\n");
+
+            for (int i = 0; i < bejegyzesek.Count; i++)
+            {
+                bejegyzesek[i].Kiir();
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
